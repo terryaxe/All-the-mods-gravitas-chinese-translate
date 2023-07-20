3c0044d09db5ef04958f1a89b05eb12d4df3dba7
Cactus_student <97865546+Cactusstudent@users.noreply.github.com>
2023-07-20 23:43:57 +0800

    接着改

--- source_files/packer.py
+++ source_files/packer.py
@@ -1,14 +1,21 @@
+import os
 import json
-import shutil
+import subprocess
 
-# 读取配置文件
-with open('source_files/packer_config.json', 'r') as f:
-    config = json.load(f)
+def run_packaging():
+    # 读取packer_config.json文件
+    with open('packer_config.json', 'r') as f:
+        config = json.load(f)
+    
+    # 获取要打包的内容或路径
+    content = config.get('content')
+    # 获取打包结果的输出路径
+    output_path = config.get('output_path')
+    
+    # 执行打包操作
+    # 根据您的需求自行编写打包逻辑，这里只是一个示例
+    command = f'your_packaging_command {content} {output_path}'
+    subprocess.run(command, shell=True, check=True)
 
-# 打包逻辑
-def package_files(paths, target_dir):
-    for path in paths:
-        shutil.make_archive(target_dir, 'zip', path)
-
-# 调用打包函数
-package_files(config['paths'], 'target_folder')
+if __name__ == '__main__':
+    run_packaging()
