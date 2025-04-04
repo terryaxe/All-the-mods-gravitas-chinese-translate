c843e605f16931cea38e28c56a6258527ebcd040
Litchiiiiiii <139540811+Litchiiiiii@users.noreply.github.com>
2025-04-04 22:27:50 +0800

    update

--- .github/workflows/para2github.py
+++ .github/workflows/para2github.py
@@ -209,20 +209,19 @@ def main() -> None:
         print(f"已从Patatranz下载到仓库：{re.sub('en_us.json', 'zh_cn.json', path)}")
     if(len(ftbquests_dict) > 0):
         snbt_dict = normal_json2_ftb_desc(ftbquests_dict)
-
         # json_data = json.dumps(snbt_dict,ensure_ascii=False, indent=4, separators=(",", ":"))
         # Escape quotation marks in the translated data
         json_data = escape_quotes(snbt_dict)
-
         # Convert the loaded JSON data to NBT format
         nbt_data = json_to_nbt(json_data)
-
         # Format the NBT structure as a pretty-printed SNBT string
         formatted_snbt_string = format_snbt(nbt_data)
         # Optionally save the formatted SNBT to a file
-        with open('CNPack/config/ftbquests/quests/lang/zh_cn.snbt', 'w', encoding='utf-8') as snbt_file:
-            snbt_file.write(formatted_snbt_string)
-
+        try:
+            with open('CNPack/config/ftbquests/quests/lang/zh_cn.snbt', 'w', encoding='utf-8') as snbt_file:
+                snbt_file.write(formatted_snbt_string)
+        except Exception as e:
+            print("该ftbquest版本低于1.21.1")
 
 if __name__ == "__main__":
     main()
